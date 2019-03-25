e5c685ab0dfd7db1437822dd60f781781f85c042
Tripp Hammond <tripphamm@gmail.com>
2019-03-24 20:10:22 -0400

    Add easier join link

--- src/routes/Routes.tsx
+++ src/routes/Routes.tsx
@@ -6,7 +6,7 @@ import TOS from '../pages/TOS';
 import Privacy from '../pages/Privacy';
 import NotFound from '../pages/NotFound';
 import Loading from '../pages/Loading';
-import { getSurveyResultsPath } from '../utils/routeUtil';
+import { getSurveyResultsPath, getSurveyQuestionPath } from '../utils/routeUtil';
 const FullAuthRoutes = React.lazy(() => import('./FullAuthRoutes'));
 const AnonymousAuthRoutes = React.lazy(() => import('./AnonymousAuthRoutes'));
 
@@ -38,6 +38,16 @@ export default function Routes() {
           const { shareCode } = params;
 
           return <Redirect to={getSurveyResultsPath(shareCode)} />;
+        }}
+      />
+      <Route
+        path="/join/:shareCode"
+        render={props => {
+          const { match } = props;
+          const { params } = match;
+          const { shareCode } = params;
+
+          return <Redirect to={getSurveyQuestionPath(shareCode)} />;
         }}
       />
       <Route path="/tos" component={TOS} />
