4481a434a49f1aaac91b51879e40d0d74a0ae918
Tripp Hammond <tripphamm@gmail.com>
2019-03-25 23:23:25 -0400

    Make fabs bigger

--- src/settings/magicNumbers.ts
+++ src/settings/magicNumbers.ts
@@ -1,7 +1,7 @@
 export const topBarHeight = 56;
 export const bottomBarHeight = 56;
 
-export const floatingActionButtonSize = 56;
+export const floatingActionButtonSize = 64;
 export const floatingActionButtonOffsetRight = 30;
 export const floatingActionButtonOffsetBottom = 30 + bottomBarHeight;
 export const floatingActionButtonBufferSize = floatingActionButtonSize + 10;
