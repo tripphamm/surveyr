2ec602c7b6bcc6c03bb02d5007a7036af31406c0
Tripp Hammond <tripphamm@gmail.com>
2019-03-31 10:23:40 -0400

    Try a circle deploy

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { StoreContext } from 'redux-react-hook';
+import rollbar from 'rollbar';
 
 import App from './App';
 import { getStore } from './state/store';
+
+rollbar.init({
+  accessToken: '4db9b76caa194b42aa3d44167bcb9d44',
+  captureUncaught: true,
+  captureUnhandledRejections: true,
+  payload: {
+    environment: process.env.NODE_ENV,
+  },
+});
 
 ReactDOM.render(
   <StoreContext.Provider value={getStore()}>
